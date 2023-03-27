3ca2a482cc89a886672989ada05f093a981a5446
SHAHRIAR AHMED <shahriar7ahmed@gmail.com>
2023-03-28 03:45:20 +0600

    Config

--- sanity.js
+++ sanity.js
@@ -3,7 +3,7 @@ import createImageUrlBuilder from "@sanity/image-url";
 
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "tq1oqm0l",
   apiVersion: "2021-03-25",
   useCdn: process.env.NODE_ENV === "production",
 };
